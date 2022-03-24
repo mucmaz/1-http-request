0e9a3ed1bbb059b8a07341f53d9e55d45e8a27d9
Mehmet UÇMAZ <mucmaz@erciyes.edu.tr>
2022-03-24 15:28:39 +0300

    vue3 route

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <hr/>
+    <div class="my-2">
+        <input type="text" placeholder="ne alacaksın" @keydown.enter="onSave" />
+    </div>
+    <ul class="list-group">
+      <li class="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
+        <span>Eleman 1</span>
+        <button class="btn btn-danger">Sil</button>
+      </li>
+            <li class="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
+        <span>Eleman 1</span>
+        <button class="btn btn-danger">Sil</button>
+      </li>
+            <li class="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
+        <span>Eleman 1</span>
+        <button class="btn btn-danger">Sil</button>
+      </li>
+            <li class="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
+        <span>Eleman 1</span>
+        <button class="btn btn-danger">Sil</button>
+      </li>
+            <li class="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
+        <span>Eleman 1</span>
+        <button class="btn btn-danger">Sil</button>
+      </li>
+    </ul>
+    <small class="badge badge-primary text-danger">5 Adet alınacak ürün vardır</small>
+    </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data (){
+    return {
+
+    }
+  },
+  methods: {
+    onSave(e){
+      const saveObject = {title:e.target.value, createdDate: new Date(), complated: false };
+      axios.post("http://localhost:3000/items", saveObject)
+      .then(response => {
+        console.log(response);
+      }).catch(err => {
+        console.log(err);
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
